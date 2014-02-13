52e27f787e5ec11ea809c18b62655c6f14657294
Clay Smith <s.clay.smith@gmail.com>
2014-02-12 21:20:00 -0800

    SUPER OPTIMIZED!!!11!

--- javascript/player.js
+++ javascript/player.js
@@ -6,22 +6,9 @@
   var TBPlayer = window.TBPlayer || {};
 
   // Set configuration variables with defaults
-  TBPlayer.config = $.extend({}, {
+  TBPlayer.config = {
     requestTarget: "http://talkingbibles.net/api/v1"
-  }, TBPlayer.config || {});
-
-  // Verify and process the configuration
-  TBPlayer.players = $.map($.makeArray(TBPlayer.players), function (player) {
-    if ("id" in player && "language" in player && "book" in player && "chapter" in player) {
-      return {
-        id: player.id,
-        reqUrl: TBPlayer.config.requestTarget + "/" + player.language + "/" + player.book + ".json",
-        chapter: player.chapter - 1
-      };
-    } else {
-      return null;
-    }
-  });
+  };
 
   // Expected API return format
   TBPlayer.expectedResponse = {
@@ -34,6 +21,7 @@
     }
   };
 
+  // Just fail gracefully, dang it!
   TBPlayer.failNicely = function (id) {
     $(id).append(
       $("<p class=\"tbplayer-failure\">The requested chapter was not found.</p>")
@@ -45,42 +33,39 @@
     var t = TBPlayer;
 
     // Iterate through chapter array creating audio players for each
-    $.each(t.players, function (i, player) {
-
-      // Create a player wrapper in place of each script tag
-      $(player.id).each(function () {
-        $(this).replaceWith("<div id=\"" + $(this).attr("id") + "\" class=\"tbplayer\"></div>");
-      });
+    $(".tbplayer").each(function (i, player) {
+      var rawData = $(player).data("chapter").split("/"),
+          chapter = rawData[2],
+          reqUrl = t.config.requestTarget + "/" + rawData[0] + "/" + rawData[1] + ".json";
 
       // Fetch the chapter information and create an audio file
       $.ajax({
-        url: player.reqUrl,
-        context: player,
+        url: reqUrl,
         dataType: "json"
       })
-        .done(function (unsafeResponse) {
-          // Make the response fit the expected format
-          var response = $.extend({}, t.expectedResponse, unsafeResponse || {});
+      .done(function (unsafeResponse) {
+        // Make the response fit the expected format
+        var response = $.extend({}, t.expectedResponse, unsafeResponse || {});
 
-          // Check for successful response and check to make sure the chapter exists
-          if (response.status === "success" && response.data.book.chapters.length > this.chapter) {
+        // Check for successful response and check to make sure the chapter exists
+        if (response.status === "success" && response.data.book.chapters.length > chapter) {
 
-            // Append a player to the player wrapper created above
-            $(this.id).append(
-              $("<audio></audio>").attr({
-                "class": "tbplayer-controls",
-                "controls": "controls",
-                "preload": "metadata",
-                src: response.data.book.chapters[this.chapter].href
-              })
-            );
-          } else {
-            t.failNicely(this.id);
-          }
-        })
-        .fail(function () {
-          t.failNicely(player.id);
-        });
+          // Append a player to the player wrapper created above
+          $(player).append(
+            $("<audio></audio>").attr({
+              "class": "tbplayer-controls",
+              "controls": "controls",
+              "preload": "metadata",
+              src: response.data.book.chapters[chapter].href
+            })
+          );
+        } else {
+          t.failNicely(player);
+        }
+      })
+      .fail(function () {
+        t.failNicely(player);
+      });
     });
 
   };
