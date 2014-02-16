8352125085d65fcac77e1c62617bc9eca0c95636
Clay Smith <s.clay.smith@gmail.com>
2014-02-15 16:03:08 -0800

    WIP on plugin.

--- dist/tbplayer.jquery.js
+++ dist/tbplayer.jquery.js
@@ -1,59 +1,72 @@
-/*global jQuery, window*/
-(function (window, $) {
+/*global jQuery*/
+(function ($) {
   "use strict";
 
-  // Create a namespace so that we're not polluting window
-  var TBPlayer = window.TBPlayer || {};
+  function TBPlayerPlugin(elem, options) {
+    var self, defaults;
 
-  // Set configuration variables
-  TBPlayer.config = {
-    requestTarget: "http://talkingbibles.net/api/v1/",
-    showFailures: true
-  };
+    // Remember this object as self
+    self = this;
 
-  TBPlayer.createPlayer = function (data) {
-    var chapterNumber = $(this).data("chapter");
-    // Check whether the chapter exists and has a private url
-    if (typeof data.chapters === "object" &&
-        typeof data.chapters[chapterNumber] === "object" &&
-        typeof data.chapters[chapterNumber].href === "string") {
-      $(this).append(
-        $("<audio></audio>").attr({
-          "class": "tbplayer-controls",
-          "controls": "controls",
-          "preload": "metadata",
-          src: data.chapters[chapterNumber].href
-        })
-      );
-    } else {
-      $(this).append(
+    // Remember the DOM element that this object is bound to
+    self.$elem = $(elem);
+
+    // Set default options
+    defaults = {
+      requestTarget: "http://talkingbibles.net/api/v1/",
+      showFailures: true
+    };
+
+    // Merge passed-in options with default options
+    self.options = $.extend({}, defaults, options);
+
+    // Get the player party started
+    init();
+
+    function init() {
+      var languageBook = self.$elem.data("book"),
+          requestUrl = self.options.requestTarget + languageBook  + ".json";
+
+      $.ajax({url: requestUrl, dataType: "json"})
+        .done(createPlayer)
+        .fail(failGracefully);
+    }
+
+    function createPlayer(data) {
+      var chapterNumber = self.$elem.data("chapter");
+      // Check whether the chapter exists and has a private url
+      if (typeof data.chapters === "object" &&
+          typeof data.chapters[chapterNumber] === "object" &&
+          typeof data.chapters[chapterNumber].href === "string") {
+        self.$elem.append(
+          $("<audio></audio>").attr({
+            "class": "tbplayer-controls",
+            "controls": "controls",
+            "preload": "metadata",
+            src: data.chapters[chapterNumber].href
+          })
+        );
+      } else {
+        self.$elem.append(
+          $("<span class=\"tbplayer-error\">The requested chapter was not found.</span>")
+        );
+      }
+    }
+
+    function failGracefully() {
+      self.$elem.append(
         $("<span class=\"tbplayer-error\">The requested chapter was not found.</span>")
       );
     }
-  };
 
-  TBPlayer.failGracefully = function() {
-    $(this).append(
-      $("<span class=\"tbplayer-error\">The requested chapter was not found.</span>")
-    );
-  };
+  }
 
-  // Do the heavy lifting
-  TBPlayer.createAll = function () {
-    var t = TBPlayer;
-
-    // Iterate through chapter array creating audio players for each
-    $(".tbplayer").each(function (i, player) {
-      var languageBook = $(player).data("book"),
-          requestUrl = TBPlayer.config.requestTarget + languageBook  + ".json";
-
-      $.ajax({url: requestUrl, context: player, dataType: "json"})
-        .done(t.createPlayer)
-        .fail(t.failGracefully);
+  $.fn.tbplayer = function (options) {
+    return this.each(function () {
+      if (!$(this).data("tbplayer")) {
+        $(this).data("tbplayer", new TBPlayerPlugin(this, options));
+      }
     });
-
   };
 
-  // Create the players once the document is ready
-  $(TBPlayer.createAll());
-})(window, jQuery);
+})(jQuery);
